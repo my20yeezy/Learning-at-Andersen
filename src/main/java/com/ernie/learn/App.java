afba48337ee68632f03b0bf8821bc70e45750b15
Ernie <yerniyaz.my20@gmail.com>
2024-09-29 03:00:52 +0500

    initial

---
+++ src/main/java/com/ernie/learn/App.java
@@ -0,0 +1,13 @@
+package com.ernie.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
